5a64b516399b06ee87dcac4b8d67601ee48c5f8a
alvaroguirs <Alvin.Rolim@perfekt.com.au>
2021-03-02 22:56:22 +1100

    added redux and implmented userReducer and userAction to Header and App components

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -35,32 +35,32 @@ class SignIn extends React.Component{
 
     render(){
         return(
-            <div className='sign-in'>
+            <div className="sign-in">
 
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
                     <FormInput 
-                        name='email' 
-                        type='email' 
+                        name="email" 
+                        type="email"
                         value={this.state.email} 
                         handleChange={this.handleChange}
-                        label='Email'
+                        label="Email"
                         required
                     />                    
 
                     <FormInput 
-                        name='password' 
-                        type='password'
+                        name="password"
+                        type="password"
                         value={this.state.password} 
                         handleChange={this.handleChange}
-                        label='Password'
+                        label="Password"
                         required
                     />
-                    <div className='buttons'>
-                        <CustomButton type='submit'>Sign In</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                    <div className="buttons">
+                        <CustomButton type="submit">Sign In</CustomButton>
+                        <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>
                             Sign In with Google
                         </CustomButton>
                     </div>
